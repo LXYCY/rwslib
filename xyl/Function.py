8f912cce7ab884eff03e91402bf18eeca5622caa
xiaoyu.li <424531307@qq.com>
2018-05-02 10:56:09 -0400

    add folder xyl.

---
+++ xyl/Function.py
@@ -0,0 +1,8 @@
+def PrintNameAndType(dictData):
+	for key, value in dictData.items():
+		print("%s type:%s" %(key, type(value)))
+
+def PrintNameAndValue(dictData):
+	for key, value in dictData.items():
+		print("%s value:%s" %(key, value))
+
